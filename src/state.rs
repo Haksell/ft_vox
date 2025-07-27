4c72756e727663c992879c31d7ab246ae6ffbd86
Haksell <axel.brisse@gmail.com>
2025-07-27 08:17:40 +0200

    wgpu 25 -> wgpu 26

--- src/state.rs
+++ src/state.rs
@@ -437,6 +437,7 @@ impl<'a> State<'a> {
                         }),
                         store: wgpu::StoreOp::Store,
                     },
+                    depth_slice: None,
                 })],
                 depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
                     view: &self.depth_texture.view,
