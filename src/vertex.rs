53fe83aef19ce4c6203d6939b9ea720ec5c37feb
Pradene <liam.pradene@gmail.com>
2025-09-05 15:26:22 +0200

    .

--- src/vertex.rs
+++ src/vertex.rs
@@ -7,17 +7,17 @@ pub struct Vertex {
 }
 
 impl Vertex {
-    pub fn desc() -> wgpu::VertexBufferLayout<'static> {
-        const ATTRIBUTES: [wgpu::VertexAttribute; 3] = wgpu::vertex_attr_array![
-            0 => Float32x3,
-            1 => Float32x2,
-            2 => Float32x2,
-        ];
+    const ATTRIBUTES: [wgpu::VertexAttribute; 3] = wgpu::vertex_attr_array![
+        0 => Float32x3,
+        1 => Float32x2,
+        2 => Float32x2,
+    ];
 
+    pub fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
             array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &ATTRIBUTES
+            attributes: &Self::ATTRIBUTES
         }
     }
 }
